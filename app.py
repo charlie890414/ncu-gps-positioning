66cab638290296a90bede856a7a2c392eefb4daf
charlie890414 <charlie890414@gmail.com>
2021-06-18 19:46:10 +0800

    finish

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import json
+app = Flask(__name__)
+
+@app.route('/')
+def index():    
+    return render_template('index.html', place_list=json.load(open('json/place.json')))
+
+@app.route('/show/<place>')
+def show(place):    
+    return render_template('information.html', place=place, detail=json.load(open(f'json/{place}.json')))
+
+@app.route('/maps/<place>')
+def maps(place):
+    return render_template(f'maps/{place}.html')
+
+if __name__ == "__main__":
+    app.run(port=9000)
--- gps.py
+++ gps.py
@@ -1,11 +1,11 @@
 import math
 import os
+import json
 
 import folium
 import pynmea2
 
-fmap = folium.Map(location=[24.968236, 121.195269], zoom_start=17)
-for point in os.listdir('data/'):
+for point in os.listdir('data/'):    
     print(point)
     file = open(f'data/{point}', encoding='utf-8')
     latitude = []
@@ -69,8 +69,10 @@ for point in os.listdir('data/'):
     print(sum(latitude)/len(latitude), sum(longitude)/len(longitude))
 
     origin_point = [sum(latitude)/len(latitude), sum(longitude)/len(longitude)]
-
-    fmap.add_child(folium.Marker(location=origin_point, popup=f"{point.split('.')[0]} <br> altitude: {(sum(altitude)/len(altitude)):.2f}"))
+    
+    fmap = folium.Map(location=origin_point, zoom_start=17, control_scale=True)
+    fmap.add_child(folium.Marker(location=origin_point, popup=f"{point.split('.')[0]}"))
+    # fmap.add_child(folium.Marker(location=origin_point, popup=f"{point.split('.')[0]} <br> altitude: {(sum(altitude)/len(altitude)):.2f}"))
 
     list_colors = [
         "#00FF00",
@@ -107,7 +109,9 @@ for point in os.listdir('data/'):
     list_colors.reverse()
     color_dict = {i: list_colors[i] for i in range(len(list_colors))}
 
-    length = .01
+    detail = {}
+
+    length = .1
     for i in SV.keys():
         azimuths = list(map(lambda x: int(x['azimuth']), SV[i]))
         snrs = list(map(lambda x: int(x['snr']), SV[i]))
@@ -118,7 +122,14 @@ for point in os.listdir('data/'):
         print(i, azimuth, snr, deg)
         end_lat = origin_point[0] + length * math.sin(math.radians(azimuth))
         end_lon = origin_point[1] + length * math.cos(math.radians(azimuth))
-        fmap.add_child(folium.PolyLine(locations=[origin_point, [end_lat, end_lon]], color=color_dict[round(snr/(50/len(color_dict)))]))
-        fmap.add_child(folium.Circle(location=[end_lat, end_lon], color="blue", fill_color="blue", fill=True, popup=f"prn: {i} <br> deg: {deg:.2f} <br> azimuth: {azimuth:.2f} <br> snr: {snr:.2f}"))
+        fmap.add_child(folium.PolyLine(locations=[origin_point, [end_lat, end_lon]], color=color_dict[round(snr/(50/len(color_dict)))], popup=f"PRN:{i}"))
+        detail[i] = {
+            'azimuth': azimuth,
+            'deg': deg,
+            'snr': snr
+        }
+        # fmap.add_child(folium.Circle(location=[end_lat, end_lon], color="blue", fill_color="blue", fill=True, popup=f"prn: {i} <br> deg: {deg:.2f} <br> azimuth: {azimuth:.2f} <br> snr: {snr:.2f}"))
+    json.dump(detail, open(f"json/{point.split('.')[0]}.json", 'w'), ensure_ascii=False)
+    fmap.save(f"templates/maps/{point.split('.')[0]}.html")
 
-fmap.save('map.html')
+json.dump(list(map(lambda x: x.split('.')[0], os.listdir('data/'))), open("json/place.json", 'w'), ensure_ascii=False)
